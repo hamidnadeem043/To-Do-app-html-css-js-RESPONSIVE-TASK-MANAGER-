e21061603e82dec7c498803d21c924a51a7ff4d4
Hamid Nadeem <hamidnadeemn043@gmail.com>
2026-05-21 16:54:51 +0500

    initial commit

---
+++ todo.js
@@ -0,0 +1,39 @@
+
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+
+
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = input.value;
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "Delete";
+    delbtn.classList.add("delete");
+
+
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    
+    input.value = "";
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON")
+    {
+        let listitem = event.target.parentElement;
+        listitem.remove();
+        console.log("item deleted");
+    
+    }});
+
+// let delbtns = document.querySelectorAll(".delete");
+
+// for(delbtn of delbtns){
+//     delbtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
